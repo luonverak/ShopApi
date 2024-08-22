ff4837877cb288f2c4226473ba0403c8c720fb27
Luon Verak <luonverakcambo@gmail.com>
2024-08-22 22:03:39 +0700

    code commit

--- web_blog/routes/web.php
+++ web_blog/routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\BackendViewsController;
 use App\Http\Controllers\FrontendViewsController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(["prefix" => "admin", "namespace" => "BackendViews"], function () {
-    Route::get("/", ["use" => "BackendViewsController@index"]);
+Route::group(["prefix" => "admin"], function () {
+    Route::get("/", [BackendViewsController::class, "index"]);
 });
-
 Route::group(["prefix" => "/"], function () {
     Route::get("/", [FrontendViewsController::class, "index"]);
 });
