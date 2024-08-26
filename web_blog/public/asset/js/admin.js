d7409715b60c6647cec0324ed950bac841a8bdf1
Luon Verak <luonverakcambo@gmail.com>
2024-08-26 21:56:30 +0700

    code commit

--- web_blog/public/asset/js/admin.js
+++ web_blog/public/asset/js/admin.js
@@ -1,4 +1,12 @@
 $(document)
     .on("click", "button.open-add-category", function () {
         $("div.category-modal").modal("show");
+    }).on("click", "div.choose-image", function () {
+        $(this).siblings("input#logo").click();
+
+        $("input#logo").on("change", function (event) {
+            let file = event.target.files[0];
+            $("div.choose-image").find("img").attr("src", URL.createObjectURL(file));
+        });
+
     });
